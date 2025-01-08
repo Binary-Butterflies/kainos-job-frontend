6eeccf974f3594d3189a81b813bc6b2e88e20a31
aaron.millen <aaron.millen@kainos.com>
2025-01-08 11:15:01 +0000

    Began Test Development

--- src/services/AuthServices.ts
+++ src/services/AuthServices.ts
@@ -2,7 +2,8 @@ import axios, {AxiosResponse} from "axios";
 import { LoginRequest } from "../models/LoginRequest";
 import { RegisterRequest } from "../models/RegisterRequest";
 
-export const getToken = async(loginRequest:LoginRequest): Promise<string> => {
+export const getToken = async function(loginRequest:LoginRequest): Promise<string> {
+    console.log(Object.getOwnPropertyDescriptor(Object.prototype, 'getToken'));
     try{
         const response:AxiosResponse = await axios.post("http://localhost:8080/api/auth/login" ,loginRequest);
         return response.data;
@@ -12,7 +13,7 @@ export const getToken = async(loginRequest:LoginRequest): Promise<string> => {
     }
 }
 
-export const createUser = async( registerRequest : RegisterRequest ):Promise<number> => {
+export const createUser = async function (registerRequest : RegisterRequest ):Promise<number> {
     try{
         const response: AxiosResponse = await axios.post("http://localhost:8080/api/auth/register", registerRequest);
         return response.data;
--- test/unit/controllers/AuthControllerTests.ts
+++ test/unit/controllers/AuthControllerTests.ts
@@ -1,28 +1,52 @@
 import * as AuthController from "../../../src/controllers/AuthController";
+import * as AuthServices from "../../../src/services/AuthServices";
 import { afterEach, describe } from "node:test"
 import sinon from 'sinon';
 import { expect } from 'chai';
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+ 
 
 declare module "express-session" {
-    interface SessionData {
-        token: string;
-    }
+  interface SessionData {
+    token: string;
+  }
 }
+describe('AuthController', function () {
+  afterEach(() => {
+    sinon.restore();
+  });
+  describe('LoginForm', function () {
+    it("should render login form", async () => {
+      const res = { render: sinon.spy() };
+      const req = {};
+      await AuthController.getLoginForm(req as Request, res as unknown as Response);
+      expect(res.render.calledOnce).to.be.true;
+    })
+    
+    it('should render login form with error message when error thrown', async () => {
+      sinon.restore();
+      const errorMessage: string = 'Error message';
+      console.log(Object.getOwnPropertyDescriptor(Object.prototype, 'getToken'));
+      sinon.stub(AuthServices, 'getToken').rejects(new Error(errorMessage));
+      console.log(Object.getOwnPropertyDescriptor(Object.prototype, 'getToken'));
+      const req = { };
+      const res = { render: sinon.spy(), locals: { errormessage: '' } };
 
-describe('AuthController', function(){
-    afterEach(()=>{
-        sinon.restore();
+      await AuthController.postLoginForm(req as Request, res as unknown as Response);
+
+      expect(res.render.calledOnce).to.be.true;
+       expect(res.locals.errormessage).to.equal(errorMessage);
+    });
+  });
+  
+  describe('RegisterForm', function () {
+    it("should render Registration form", async () => {
+      const res = { render: sinon.spy() };
+      const req = {};
+      await AuthController.getRegistrationForm(req as Request, res as unknown as Response);
+      expect(res.render.calledOnce).to.be.true;
     });
 
-    describe('LoginForm' , function(){
-        it("should render login form", async() => {
-            const res = { render: sinon.spy() };
-            const req = { };
+  });
+})
 
-            await AuthController.getLoginForm(req as Request, res as unknown as Response);
-
-            expect(res.render.calledOnce).to.be.true;
-        })
-    })
-}) 
