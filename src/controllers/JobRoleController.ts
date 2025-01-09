09b6fb43ae0f862d43bce7cb41765903aea15741
CForde-Kainos <connor.forde@kainos.com>
2025-01-09 16:01:09 +0000

    001-view-job-roles fix actual final

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -7,9 +7,9 @@ export const getIndex = async (req: express.Request, res: express.Response): Pro
 
 export const getJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        res.render('list-jobroles', { jobRoles: await getAllJobRoles() });
+        res.render('jobroles-list', { jobRoles: await getAllJobRoles() });
     } catch (e) {
         res.locals.errormessage = e.message;
-        res.render('list-jobroles');
+        res.render('jobroles-list');
     }
 };
