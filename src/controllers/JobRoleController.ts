c2714f3bd703aa94a2985a1d5e38714db9c30453
Matthew Jordan <matthew.jordan@kainos.com>
2025-01-13 12:30:00 +0000

    US041: Added Authorisation

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -7,7 +7,7 @@ export const getIndex = async (req: express.Request, res: express.Response): Pro
 
 export const getJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
-        res.render('jobroles-list', { jobRoles: await getAllJobRoles() });
+        res.render('jobroles-list', { jobRoles: await getAllJobRoles(req.session.token) });
     } catch (e) {
         res.locals.errormessage = e.message;
         res.render('jobroles-list');
